8ac8a5e47dfabcedd562846019781843b3157e7c
Kiryo <129882869+Kiryo2101@users.noreply.github.com>
2023-10-14 07:42:34 +0700

    Update ThemeManager.lua

--- LinoriaLib/addons/ThemeManager.lua
+++ LinoriaLib/addons/ThemeManager.lua
@@ -14,7 +14,7 @@ local ThemeManager = {} do
 		['Tokyo Night'] 	= { 7, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"191925","AccentColor":"6759b3","BackgroundColor":"16161f","OutlineColor":"323232"}') },
 		['Ubuntu'] 			= { 8, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"3e3e3e","AccentColor":"e2581e","BackgroundColor":"323232","OutlineColor":"191919"}') },
 		['Quartz'] 			= { 9, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"232330","AccentColor":"426e87","BackgroundColor":"1d1b26","OutlineColor":"27232f"}') },
-		['Halloween']		= { 10, httpService:JSONDecode('{"MainColor":"000000","AccentColor":"85e21e","OutlineColor":"881de4","BackgroundColor":"f75f1c","FontColor":"ffffff"}') }
+		['Halloween']		= { 10, httpService:JSONDecode('{"MainColor":"000000","AccentColor":"85e21e","OutlineColor":"881de4","BackgroundColor":"f96d30","FontColor":"ffffff"}') }
 	}
 
 	function ThemeManager:ApplyTheme(theme)
