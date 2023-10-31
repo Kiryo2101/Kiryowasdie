143e5eca64a9316e94214d6cdb84e683c4042ed5
Kiryo <129882869+Kiryo2101@users.noreply.github.com>
2023-10-31 19:50:14 +0700

    Update Fluent.lua

--- Fluent.lua
+++ Fluent.lua
@@ -2,14 +2,4 @@ local Lib = {}
 local Fluent = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
 local SaveManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/SaveManager.lua"))()
 local InterfaceManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/InterfaceManager.lua"))()
-
-local Window = Fluent:CreateWindow({
-    Title = "Kiryowasdie.",
-    SubTitle = "by Kiryo",
-    TabWidth = 160,
-    Size = UDim2.fromOffset(580, 460),
-    Acrylic = true,
-    Theme = "Dark",
-    MinimizeKey = Enum.KeyCode.LeftControl
-})
 return Lib
